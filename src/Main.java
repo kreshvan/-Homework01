6d1ab4a83bea445a7d4e5f0a1723a90ba0e319b9
днс <kreshvan12317@yandex.ru>
2024-05-03 16:38:08 +0300

    «Cделано ДЗ» commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+
+
+    //vpublic static void main(String[] args) {
+    //        System.out.println("Hello Word!");
+
     public static void main(String[] args) {
+
         System.out.println("Привет Мир!");
     }
 }
